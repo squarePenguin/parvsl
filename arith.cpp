85a4546e046de11b2bac767bf8f53325a5c76904
arthurcnorman <acn1@cam.ac.uk>
2018-10-20 20:41:54 +0100

    Work towards high precision integer arithmetic using 64-bit digits. + and - and some bitwise operators more or less done!

--- arith.cpp
+++ arith.cpp
@@ -108,7 +108,7 @@ static inline LispObject *heapaddr(LispObject x)
 
 
 static inline LispObject boxfloat(double a)
-{   LispObject r;
+{  // LispObject r;
 //    check_space(8, __LINE__);
 //    setheapstartsandfp(fringe1);
 //    r = fringe1 + tagFLOAT;
@@ -118,17 +118,71 @@ static inline LispObject boxfloat(double a)
     return 0;
 }
 
-static LispObject error0(const char *mag)
-{   return 0;
-}
+//static LispObject error0(const char *msg)
+//{   return 0;
+//}
 
-static LispObject error1(const char *mag, LispObject a)
-{   return 0;
-}
+//static LispObject error1(const char *msg, LispObject a)
+//{   return 0;
+//}
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-static const uint64_t allbits = ~(uint64_t)0;
+// I am going to represent bignums as arrays of 64-bit digits.
+// Overall the representation will use 2s complement, and so all but the
+// top digit will be treated as unsigned, while the top one is signed
+// and the whole number must act as if it had its sign bit propagated
+// indefinitely to the left. When I pass numbers to the low level
+// code I will pass references to the input arrays and lengths. I will
+// pass an arrange that will certainly be large enough to hold the result
+// and the arithmetic functions will return the length in it that is used.
+// This length will be such that the overall number does not have any
+// extraneous leading zeros or leading 0xffffffffffffffff words, save that
+// the value zero will be returned as a single word value not a no-word
+// one. A consequence of all this is that any bignum with length 1 can be
+// extracted as an int64_t without loss.
+
+// I want "add-with-carry" operations, and so I provide a function here to
+// implement it. If the C++ compiler had a nice intrinsic I would like
+// to use that! Well Intel compilers have an _addcarry_u64 that passes and
+// returns the carry in an unsigned char and uses a pointer not a reference
+// argument for passing back the result.
+
+// a1 and a2 are 64-bit unsigned integers. While c_in is also that type it
+// must only have one of the values 0 or 1. The effect will be to set r to
+// the low 64-bits of a1+a2+c_in and return any carry that is generated.
+
+static inline uint64_t add_with_carry(uint64_t a1, uint64_t a2,
+                                      uint64_t c_in, uint64_t &r)
+{   uint64_t w = a1 + c_in;
+    if (w < c_in) // carry generated here. In this case the only possibility
+    {             // was that a1 was allbits and c_in was 1, and so we have
+                  // ended up with w==0. So the result sum is just a2 and the
+                  // carry out will be 1
+        r = a2;
+        return 1;
+    }
+    r = w = w + a2;
+    return (w < a2 ? 1 : 0);
+}
+
+// I have an overload of add_with_carry for use where it is known that
+// the input carry is zero. That cases saves a small amount of work.
+
+static inline uint64_t add_with_carry(uint64_t a1, uint64_t a2, uint64_t &r)
+{   uint64_t w;
+    r = w = a1 + a2;
+    return (w < a1 ? 1 : 0);
+}
+
+
+// While my arithmetic is all done in uint64_t (and that is important so
+// that in C++ the consequences of overflow are defined) I need to treat
+// some top-digits as signed: here are values and tests relating to that.
+
+static const uint64_t allbits   = ~(uint64_t)0;
+static const uint64_t topbit    = ((uint64_t)1)<<63;
+static const uint64_t allbuttop = topbit - 1;
 
 static inline bool positive(uint64_t a)
 {   return ((int64_t)a) >= 0;
@@ -138,40 +192,265 @@ static inline bool negative(uint64_t a)
 {   return ((int64_t)a) < 0;
 }
 
-// Add when the length of a is greater than that of b.
-size_t bigadd1(uint64_t *a, size_t lena, uint64_t *b, size_t lenb, uint64_t *c)
-{   uint64_t carry;
-    size_t i;
-// First add the digits that (a) and (b) have in common
-    for (i=0; i<lenb; i++)
-    {   carry = a[i] + b[i] + carry;
-        c[i] = carry;
-        carry = (carry < a[i] ? 1 : 0);
+// Conversion from a simple int64_t value.
+
+void int_to_bignum(int64_t n, uint64_t *r, size_t &lenr)
+{   r[0] = (uint64_t)n;
+    lenr = 1;                     // Always of length 1
+}
+
+// Negation. Note that because I am using 2s complement the result could be
+// one word longer or shorter than the input. For instance if you negate
+// [0x8000000000000000] (a negative value) you get [0,0x8000000000000000],
+// and vice versa.
+
+void bignegate(const uint64_t *a, size_t lena, uint64_t *r, size_t &lenr)
+{   uint64_t carry = 1;
+    for (size_t i=0; i<lena; i++)
+    {   carry = ~a[i] + carry;
+        r[i] = carry;
+        carry = (carry == 0 ? 1 : 0);
     }
+// The next digit up is really now (carry + (negative(a[i-1]) ? allbits : 0))
+// but because the length-changing cases are very special here I can simplify
+// the tests that I do to detect them. If the top digit of the result is now
+// 0x8000000000000000 then it must now be positive and I must extend the
+// number, while if it is 0 and the next digit down now looks positive
+// I can shrink.
+    if (r[lena-1]==topbit) r[lena++] = 0;
+    else if (r[lena-1]==0 && lena>1 && positive(r[lena-2])) lena--; 
+    lenr = lena;
+}
+
+// The "bitnot" operation is simple and length can not change.
+
+void biglognot(const uint64_t *a, size_t lena, uint64_t *r, size_t &lenr)
+{   for (size_t i=0; i<lena; i++)
+    {   r[i] = ~a[i];
+    }
+    lenr = lena;
+}
+
+// logand
+
+void ordered_biglogand(const uint64_t *a, size_t lena,
+                       const uint64_t *b, size_t lenb,
+                       uint64_t *r, size_t &lenr)
+{   for (size_t i=0; i<lenb; i++)
+        r[i] = a[i] & b[i];
+    if (negative(b[lenb-1])) lenr = lena;
+    else lenr = lenb;
+}
+
+void biglogand(const uint64_t *a, size_t lena,
+               const uint64_t *b, size_t lenb,
+               uint64_t *r, size_t &lenr)
+{   if (lena >= lenb) return ordered_biglogand(a, lena, b, lenb, r, lenr);
+    else return ordered_biglogand(b, lenb, a, lena, r, lenr);
+}
+
+// logor
+
+void ordered_biglogor(const uint64_t *a, size_t lena,
+                      const uint64_t *b, size_t lenb,
+                      uint64_t *r, size_t &lenr)
+{   for (size_t i=0; i<lenb; i++)
+        r[i] = a[i] | b[i];
+    if (negative(b[lenb-1])) lenr = lenb;
+    else lenr = lena;
+}
+
+void biglogor(const uint64_t *a, size_t lena,
+              const uint64_t *b, size_t lenb,
+              uint64_t *r, size_t &lenr)
+{   if (lena >= lenb) return ordered_biglogor(a, lena, b, lenb, r, lenr);
+    else return ordered_biglogor(b, lenb, a, lena, r, lenr);
+}
+
+// logxor
+
+void ordered_biglogxor(const uint64_t *a, size_t lena,
+                       const uint64_t *b, size_t lenb,
+                       uint64_t *r, size_t &lenr)
+{   size_t i;
+    for (i=0; i<lenb; i++)
+        r[i] = a[i] ^ b[i];
+    if (negative(b[lenb-1]))
+    {   for (; i<lena; i++)
+            r[i] = ~a[i];
+    }
+// The logxor operation can cause the inputs to shrink.
+    while (r[lena-1]==0 && lena>1 && positive(r[lena-2])) lena--;
+    while (r[lena-1]==allbits && lena>1 && negative(r[lena-2])) lena--;
+    lenr = lena;
+}
+
+void biglogxor(const uint64_t *a, size_t lena,
+               const uint64_t *b, size_t lenb,
+               uint64_t *r, size_t &lenr)
+{   if (lena >= lenb) return ordered_biglogxor(a, lena, b, lenb, r, lenr);
+    else return ordered_biglogxor(b, lenb, a, lena, r, lenr);
+}
+
+// Add when the length of a is greater than that of b.
+
+static inline void ordered_bigadd(const uint64_t *a, size_t lena,
+                                  const uint64_t *b, size_t lenb,
+                                  uint64_t *r, size_t &lenr)
+{   uint64_t carry = 0;
+    size_t i = 0;
+// The lowest digits can be added without there being any carry-in.
+    carry = add_with_carry(a[0], b[0], r[0]);
+// Add the digits that (a) and (b) have in common
+    for (i=1; i<lenb; i++)
+        carry = add_with_carry(a[i], b[i], carry, r[i]);
 // From there on up treat (b) as if it had its sign bit extended to the
 // left.
     uint64_t topb = negative(b[lenb-1]) ? allbits : 0;
     for (; i<lena; i++)
-    {   carry = a[i] + topb + carry;
-        c[i] = carry;
-        carry = (carry < a[i] ? 1 : 0);
-    }
-// And of course (a) must also be treated as being extended bt its sign bit.
-    uint64_t topa = begative(a[lena-1]) ? allbits : 0;
+        carry = add_with_carry(a[i], topb, carry, r[i]);
+// And of course (a) must also be treated as being extended by its sign bit.
+    uint64_t topa = negative(a[lena-1]) ? allbits : 0;
 // The result calculated here is 1 word longer than (a), and addition
 // can never carry further than that.
-    c[i] = topa + topb + carry;
+    r[i] = topa + topb + carry;
 // However because I am using (2s complement) signed arithmetic the result
 // could be shorter, so I will check for that and return the length that
 // is actually needed.
-    while (c[i]==0 && i>0 && positive(c[i-1]) i--;
-    while (c[i]==allbits && i>0 && negative(c[i-1]) i--;
-    return i;
+    while (r[i]==0 && i>0 && positive(r[i-1])) i--;
+    while (r[i]==allbits && i>0 && negative(r[i-1])) i--;
+    lenr = i+1;
 }
 
-size_t bigadd(uint64_t *a, size_t lena, uint64_t *b, size_t lenb, uint64_t *c)
-{   if (lena >= lenb) return bigadd1(a, lena, b, lenb, c);
-    else return bigadd1(b, lenb, a, lena, c);
+// Add a small number to a bignum
+
+void bigadd_small(const uint64_t *a, size_t lena,
+                  int64_t n,
+                  uint64_t *r, size_t &lenr)
+{   uint64_t w[1];
+    w[0] = (uint64_t)n;
+    ordered_bigadd(a, lena, w, 1, r, lenr);
+}
+
+// When I do a general addition I will not know which input is longer.
+
+void bigadd(const uint64_t *a, size_t lena,
+            const uint64_t *b, size_t lenb,
+            uint64_t *r, size_t &lenr)
+{   if (lena >= lenb) return ordered_bigadd(a, lena, b, lenb, r, lenr);
+    else return ordered_bigadd(b, lenb, a, lena, r, lenr);
+}
+
+// For subtraction I implement both a-b and b-a. These work by
+// computing a + (~b) + 1 and (~a) + b + 1 respectively.
+
+static inline void ordered_bigsubtract(const uint64_t *a, size_t lena,
+                                       const uint64_t *b, size_t lenb,
+                                       uint64_t *r, size_t &lenr)
+{   uint64_t carry = 1;
+    size_t i;
+// Add the digits that (a) and (b) have in common
+    for (i=0; i<lenb; i++)
+        carry = add_with_carry(a[i], ~b[i], carry, r[i]);
+// From there on up treat (b) as if it had its sign bit extended to the
+// left.
+    uint64_t topb = negative(~b[lenb-1]) ? allbits : 0;
+    for (; i<lena; i++)
+        carry = add_with_carry(a[i], topb, carry, r[i]);
+// And of course (a) must also be treated as being extended by its sign bit.
+    uint64_t topa = negative(a[lena-1]) ? allbits : 0;
+// The result calculated here is 1 word longer than (a), and addition
+// can never carry further than that.
+    r[i] = topa + topb + carry;
+// However because I am using (2s complement) signed arithmetic the result
+// could be shorter, so I will check for that and return the length that
+// is actually needed.
+    while (r[i]==0 && i>0 && positive(r[i-1])) i--;
+    while (r[i]==allbits && i>0 && negative(r[i-1])) i--;
+    lenr = i+1;
+}
+
+static inline void ordered_bigrevsubtract(const uint64_t *a, size_t lena,
+                                          const uint64_t *b, size_t lenb,
+                                          uint64_t *r, size_t &lenr)
+{   uint64_t carry = 1;
+    size_t i;
+// Add the digits that (a) and (b) have in common
+    for (i=0; i<lenb; i++)
+        carry = add_with_carry(~a[i], b[i], carry, r[i]);
+// From there on up treat (b) as if it had its sign bit extended to the
+// left.
+    uint64_t topb = negative(b[lenb-1]) ? allbits : 0;
+    for (; i<lena; i++)
+        carry = add_with_carry(~a[i], topb, carry, r[i]);
+// And of course (a) must also be treated as being extended by its sign bit.
+    uint64_t topa = negative(~a[lena-1]) ? allbits : 0;
+// The result calculated here is 1 word longer than (a), and addition
+// can never carry further than that.
+    r[i] = topa + topb + carry;
+// However because I am using (2s complement) signed arithmetic the result
+// could be shorter, so I will check for that and return the length that
+// is actually needed.
+    while (r[i]==0 && i>0 && positive(r[i-1])) i--;
+    while (r[i]==allbits && i>0 && negative(r[i-1])) i--;
+    lenr = i+1;
+}
+
+// Subtract a small number from a bignum
+
+void bigsubtract_small(const uint64_t *a, size_t lena,
+                       int64_t n,
+                       uint64_t *r, size_t &lenr)
+{   uint64_t w[1];
+    w[0] = (uint64_t)n;
+    ordered_bigsubtract(a, lena, w, 1, r, lenr);
+}
+
+// subtract a bignum from a small number
+
+void bigrevsubtract_small(const uint64_t *a, size_t lena,
+                          int64_t n,
+                          uint64_t *r, size_t &lenr)
+{   uint64_t w[1];
+    w[0] = (uint64_t)n;
+    ordered_bigrevsubtract(a, lena, w, 1, r, lenr);
+}
+
+
+void bigsubtract(const uint64_t *a, size_t lena,
+                 const uint64_t *b, size_t lenb,
+                 uint64_t *r, size_t &lenr)
+{   if (lena >= lenb) return ordered_bigsubtract(a, lena, b, lenb, r, lenr);
+    else return ordered_bigrevsubtract(b, lenb, a, lena, r, lenr);
+}
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// I need some test code for all of this!
+
+void display(const char *label, uint64_t *a, size_t lena)
+{   printf("%s: [%d]", label, (int)lena);
+    for (size_t i=0; i<lena; i++)
+        printf(" %.16" PRIx64, a[lena-i-1]);
+    printf("\n");
+}
+
+uint64_t a[100], b[100], c[100];
+size_t lena, lenb, lenc;
+
+int main(int argc, char *argv[])
+{
+    int_to_bignum(allbuttop, a, lena);
+    display("a", a, lena);
+    int_to_bignum(1, b, lenb);
+    display("b", b, lenb);
+    bigadd(a, lena, b, lenb, c, lenc);
+    display("c", c, lenc);
+    int_to_bignum(-1, b, lenb);
+    display("b", b, lenb);
+    bigadd(c, lenc, b, lenb, a, lena);
+    display("a", a, lena);
+    return 0;    
 }
 
 
